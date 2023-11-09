83d732572fd9bc4bf67b25fc10d98ee633fbb6a8
Victor C <victor.ched@gmail.com>
2023-11-08 22:12:48 -0500

    Link git branch name to the file swapper

--- UnityLib.py
+++ UnityLib.py
@@ -3,20 +3,13 @@ import shutil
 import subprocess
 
 # Define the paths
-new_branch_name = ".Release"
-old_branch_name = ".Develop"
-
 unity__project_path = "C:/Users/Number/Documents/Git/UnityProject"
 unity_library_path = "C:/Users/Number/Documents/Git/UnityProject/Library"
 library_repo_path_temp = "C:/Users/Number/Documents/Git/UnityLibraryRepo/Temp/Library"
 library_repo_path_backup = "C:/Users/Number/Documents/Git/UnityLibraryRepo/Backup/Library"
 
-old_library_temp_path = library_repo_path_temp + old_branch_name
-old_library_backup_path = library_repo_path_backup + old_branch_name
-new_library_backup_path = library_repo_path_backup + new_branch_name
-
 try:
-    # Run the 'git rev-parse --symbolic-full-name @{-1}' command to get the previous branch name
+    # Get Cutent and previous git branch name
     current_branch_result = subprocess.run(['git', 'rev-parse', '--symbolic-full-name', 'HEAD'], cwd=unity__project_path, stdout=subprocess.PIPE, text=True, check=True)
     current_branch_path = current_branch_result.stdout.strip()
     current_branch_name = current_branch_path.split("/")[-1]
@@ -32,6 +25,13 @@ except subprocess.CalledProcessError as e:
     print(f'Error running the Git command: {e.stderr}')
 except FileNotFoundError:
     print('Git executable not found. Please make sure Git is installed and available in your system PATH.')
+
+new_branch_name = "." + current_branch_name
+old_branch_name = "." + previous_branch_name
+
+old_library_temp_path = library_repo_path_temp + old_branch_name
+old_library_backup_path = library_repo_path_backup + old_branch_name
+new_library_backup_path = library_repo_path_backup + new_branch_name
 
 if os.path.exists(old_library_temp_path):
     shutil.rmtree(old_library_temp_path)
