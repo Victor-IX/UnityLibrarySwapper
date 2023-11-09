545b098f10252664b39a20ca145522bbdd26c8db
Victor C <victor.ched@gmail.com>
2023-11-08 22:05:44 -0500

    Integrate Git Name Branch pull

--- UnityLib.py
+++ UnityLib.py
@@ -1,11 +1,13 @@
 import os
 import shutil
+import subprocess
 
 # Define the paths
 new_branch_name = ".Release"
 old_branch_name = ".Develop"
 
-unity_project_path = "C:/Users/Number/Documents/Git/UnityProject/Library"
+unity__project_path = "C:/Users/Number/Documents/Git/UnityProject"
+unity_library_path = "C:/Users/Number/Documents/Git/UnityProject/Library"
 library_repo_path_temp = "C:/Users/Number/Documents/Git/UnityLibraryRepo/Temp/Library"
 library_repo_path_backup = "C:/Users/Number/Documents/Git/UnityLibraryRepo/Backup/Library"
 
@@ -13,16 +15,34 @@ old_library_temp_path = library_repo_path_temp + old_branch_name
 old_library_backup_path = library_repo_path_backup + old_branch_name
 new_library_backup_path = library_repo_path_backup + new_branch_name
 
+try:
+    # Run the 'git rev-parse --symbolic-full-name @{-1}' command to get the previous branch name
+    current_branch_result = subprocess.run(['git', 'rev-parse', '--symbolic-full-name', 'HEAD'], cwd=unity__project_path, stdout=subprocess.PIPE, text=True, check=True)
+    current_branch_path = current_branch_result.stdout.strip()
+    current_branch_name = current_branch_path.split("/")[-1]
+    
+    previous_branch_result = subprocess.run(['git', 'rev-parse', '--symbolic-full-name', '@{-1}'], cwd=unity__project_path, stdout=subprocess.PIPE, text=True, check=True)
+    previous_branch_path = previous_branch_result.stdout.strip()
+    previous_branch_name = previous_branch_path.split("/")[-1]
+
+    print(previous_branch_name)
+    print(current_branch_name)
+
+except subprocess.CalledProcessError as e:
+    print(f'Error running the Git command: {e.stderr}')
+except FileNotFoundError:
+    print('Git executable not found. Please make sure Git is installed and available in your system PATH.')
+
 if os.path.exists(old_library_temp_path):
     shutil.rmtree(old_library_temp_path)
 
-shutil.copytree(unity_project_path, old_library_temp_path) # copy past old
-shutil.rmtree(unity_project_path) # delet
+shutil.copytree(unity_library_path, old_library_temp_path) # copy past old
+shutil.rmtree(unity_library_path) # delet
 
-if os.path.exists(unity_project_path):
-    shutil.rmtree(unity_project_path)
+if os.path.exists(unity_library_path):
+    shutil.rmtree(unity_library_path)
 
-shutil.copytree(new_library_backup_path, unity_project_path) # copy past new
+shutil.copytree(new_library_backup_path, unity_library_path) # copy past new
 
 if os.path.exists(old_library_backup_path):
     shutil.rmtree(old_library_backup_path)
